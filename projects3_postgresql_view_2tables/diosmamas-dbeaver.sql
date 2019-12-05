db332000efde3b1f2a0b21439162db73855a9735
flpedro <flpedro@student.apc.edu.ph>
2019-12-05 14:54:48 +0800

    Added dump backup posgre and updated sql

--- projects3_postgresql_view_2tables/diosmamas-dbeaver.sql
+++ projects3_postgresql_view_2tables/diosmamas-dbeaver.sql
@@ -2,14 +2,14 @@ CREATE TABLE flpedro.agents (
     id serial PRIMARY KEY,
     fName varchar(45) NOT NULL,
     lName varchar(45) NOT NULL,
-    cNum int NOT NULL
+    cNum varchar(45) NOT NULL
 );
 
 CREATE TABLE flpedro.customers (
     id serial PRIMARY KEY,
     fName varchar(45) NOT NULL,
     lName varchar(45) NOT NULL,
-    cNum int NOT NULL,
+    cNum varchar(45) NOT NULL,
     typeCustomer varchar(45) NOT NULL,
     agent_id int NOT NULL,
     customerCol varchar(45) NOT NULL,
@@ -20,14 +20,14 @@ CREATE TABLE flpedro.caterers (
     id serial PRIMARY KEY,
     catComp varchar(45) NOT NULL,
     package varchar(45) NOT NULL,
-    cNum int NOT NULL
+    cNum varchar(45) NOT NULL
 );
 
 CREATE TABLE flpedro.managers (
     id serial PRIMARY KEY,
     fName varchar(45) NOT NULL,
     lName varchar(45) NOT NULL,
-    cNum int NOT NULL,
+    cNum varchar(45) NOT NULL,
     caterer_id int NOT NULL,
     FOREIGN KEY (caterer_id) REFERENCES flpedro.caterers (id)
 );
@@ -51,7 +51,7 @@ CREATE TABLE flpedro.photographers (
     nameP varchar(45) NOT NULL,
     package varchar(45) NOT NULL,
     photographerCol varchar(45) NOT NULL,
-    cNum int NOT NULL,
+    cNum varchar(45) NOT NULL,
     manager_id int NOT NULL,
     FOREIGN KEY (manager_id) REFERENCES flpedro.managers (id)
 );
