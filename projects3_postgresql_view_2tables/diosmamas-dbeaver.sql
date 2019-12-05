1b0563537758f7427d26890d564e3c1e85afd889
flpedro <flpedro@student.apc.edu.ph>
2019-12-05 14:33:19 +0800

    SQL Backup for dbeaver

---
+++ projects3_postgresql_view_2tables/diosmamas-dbeaver.sql
@@ -0,0 +1,57 @@
+CREATE TABLE flpedro.agents (
+    id serial PRIMARY KEY,
+    fName varchar(45) NOT NULL,
+    lName varchar(45) NOT NULL,
+    cNum int NOT NULL
+);
+
+CREATE TABLE flpedro.customers (
+    id serial PRIMARY KEY,
+    fName varchar(45) NOT NULL,
+    lName varchar(45) NOT NULL,
+    cNum int NOT NULL,
+    typeCustomer varchar(45) NOT NULL,
+    agent_id int NOT NULL,
+    customerCol varchar(45) NOT NULL,
+    FOREIGN KEY (agent_id) REFERENCES flpedro.agents (id)
+);
+
+CREATE TABLE flpedro.caterers (
+    id serial PRIMARY KEY,
+    catComp varchar(45) NOT NULL,
+    package varchar(45) NOT NULL,
+    cNum int NOT NULL
+);
+
+CREATE TABLE flpedro.managers (
+    id serial PRIMARY KEY,
+    fName varchar(45) NOT NULL,
+    lName varchar(45) NOT NULL,
+    cNum int NOT NULL,
+    caterer_id int NOT NULL,
+    FOREIGN KEY (caterer_id) REFERENCES flpedro.caterers (id)
+);
+
+CREATE TABLE flpedro.reservations (
+    id serial PRIMARY KEY,
+    dateR date NOT NULL,
+    eventR varchar(45) NOT NULL,
+    venueR varchar(45) NOT NULL,
+    customer_id int NOT NULL,
+    agent_id int NOT NULL,
+    manager_id int NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES flpedro.customers (id),
+    FOREIGN KEY (agent_id) REFERENCES flpedro.agents (id),
+    FOREIGN KEY (manager_id) REFERENCES flpedro.managers (id)
+    );
+
+CREATE TABLE flpedro.photographers (
+    id serial PRIMARY KEY,
+    compP varchar(45) NOT NULL,
+    nameP varchar(45) NOT NULL,
+    package varchar(45) NOT NULL,
+    photographerCol varchar(45) NOT NULL,
+    cNum int NOT NULL,
+    manager_id int NOT NULL,
+    FOREIGN KEY (manager_id) REFERENCES flpedro.managers (id)
+);
